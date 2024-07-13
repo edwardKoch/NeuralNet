dfd9bbc402c8410a435b072f3811863fbe1bfefa
Edward <edwardKoch598@gmail.com>
2024-07-12 21:56:57 -0700

    Removed Perceptron to prepare for matrix upgrades

--- NeuralNet.h
+++ NeuralNet.h
@@ -13,7 +13,11 @@
 #ifndef NEURAL_NET_H
 #define NEURAL_NET_H
 
-#include "Perceptron.h"
+#include <ctime>
+#include <math.h>
+#include <random>
+#include <stdint.h>
+#include <vector>
 
 template <uint16_t numInputs, uint16_t numHidden, uint16_t numOutputs>
 class NeuralNet
@@ -30,78 +34,33 @@ public:
     void print();
 
 private:
-    // Each Input Node has 1 input
-    std::vector<Perceptron<1>> inputLayer;
+    // Random Number Generator
+    std::mt19937 rng;
+    std::uniform_real_distribution<float_t> uniformDist;
 
-    // Each Hidden Node takes in an input from each Input node
-    std::vector<Perceptron<numInputs>> hiddenLayer;
-
-    // Each Output Node takes in an input from each Hidden node
-    std::vector<Perceptron<numHidden>> outputLayer;
-
+    // Learning Rate
     float_t learningRate;
 };
 
 template <uint16_t numInputs, uint16_t numHidden, uint16_t numOutputs>
 NeuralNet<numInputs, numHidden, numOutputs>::NeuralNet(float_t learningRate)
-    : learningRate(learningRate)
+    : rng((uint32_t)std::time(0)),
+      uniformDist(-1.0, 1.0),
+      learningRate(learningRate)
 {
-    inputLayer.reserve(numInputs);
-    for (int i = 0; i < numInputs; ++i)
-    {
-        inputLayer.push_back(Perceptron(learningRate));
-    }
 
-    hiddenLayer.reserve(numHidden);
-    for (int i = 0; i < numHidden; ++i)
-    {
-        hiddenLayer.push_back(Perceptron(learningRate));
-    }
+}
 
-    outputLayer.reserve(numOutputs);
-    for (int i = 0; i < numOutputs; ++i)
-    {
-        outputLayer.push_back(Perceptron(learningRate));
-    }
+template <uint16_t numInputs, uint16_t numHidden, uint16_t numOutputs>
+NeuralNet<numInputs, numHidden, numOutputs>::~NeuralNet()
+{
+
 }
 
 template <uint16_t numInputs, uint16_t numHidden, uint16_t numOutputs>
 std::vector<float_t> NeuralNet<numInputs, numHidden, numOutputs>::guess(std::vector<float_t> inputs)
 {
-    // Initialize result to all 0s
-    std::vector<float_t> result;
-    for (int i = 0; i < numOutputs; ++i)
-    {
-        result.push_back(0.0);
-    }
 
-    if (inputs.size() != numInputs)
-    {
-        return result;
-    }
-
-    // Get the sum of all input nodes
-    std::vector<float_t> inputLayerGuess;
-    for (int i = 0; i < numInputs; ++i)
-    {
-        std::vector<float_t> input = { inputs[i] };
-        inputLayerGuess.push_back(inputLayer[i].guess(input));
-    }
-
-    // Feed inputs forward to Hidden Layer
-    std::vector<float_t> hiddenLayerGuess;
-    for (int i = 0; i < numHidden; ++i)
-    {
-        hiddenLayerGuess.push_back(hiddenLayer[i].guess(inputLayerGuess));
-    }
-
-    // Get result from Output Layer
-    for (int i = 0; i < numOutputs; ++i)
-    {
-        result[i] = outputLayer[i].guess(hiddenLayerGuess);
-    }
-
-    return result;
 }
 
 template <uint16_t numInputs, uint16_t numHidden, uint16_t numOutputs>
--- main.cpp
+++ main.cpp
@@ -1,65 +1,10 @@
-#include "Perceptron.h"
 #include "NeuralNet.h"
 int main()
 {
-    // Random Number Generator - included from Perceptron
+    // Random Number Generator
     std::mt19937 rng((uint32_t)std::time(0));
     std::uniform_real_distribution<float> uniformDist(-1.0, 1.0);
 
-    Perceptron<2> binaryP(0.1);
+    NeuralNet<2, 2, 1> brain(0.1);
 
-    // Sample Training Set
-    uint16_t correct = 0;
-    uint16_t total = 1000;
-
-    float_t x = 0.0;
-    float_t y = 0.0;
-    float_t label = 0.0;
-
-    for (int i = 0; i < total; ++i)
-    {
-        x = uniformDist(rng);
-        y = uniformDist(rng);
-
-        std::vector<float_t> traingSet = { x, y };
-        if (x > y)
-        {
-            label = 1;
-        }
-        else
-        {
-            label = -1;
-        }
-
-        binaryP.train(traingSet, label);
-
-        // Test Perceptron
-        x = uniformDist(rng);
-        y = uniformDist(rng);
-        std::vector<float_t> testSet = { x, y };
-        if (x > y)
-        {
-            label = 1;
-        }
-        else
-        {
-            label = -1;
-        }
-        float_t guess = binaryP.guess(testSet);
-
-        //printf("Guess: %.6f, Label: %.6f\n", guess, label);
-
-        if (guess - label < 0.000005)
-        {
-            printf("Correct!\n");
-            ++correct;
-        }
-        else
-        {
-            printf("FAIL! Error: %.6f, Inputs: %.2f, %.2f\n", guess - label, x, y);
-        }
-    }
-
-    printf("Success Rate: %.2f%%\n", float_t(correct) / (total) * 100);
-    binaryP.print();
 }
